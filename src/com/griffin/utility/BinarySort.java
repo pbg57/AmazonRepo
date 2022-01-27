cc9f287cc40c5577b325d582a051b86ac819b207
Phil Griffin <pbg57@yahoo.com>
2022-01-27 16:55:48 -0700

    Amazon cleanup

--- src/com/griffin/utility/BinarySort.java
+++ src/com/griffin/utility/BinarySort.java
@@ -4,19 +4,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class BinarySort {
-
+/*
+Write a binary sort class to improve search time across an ordered array of integers.
+The array may contain duplicate entries, so binary searches should return first and last
+occurrences.
+ */
     public static void main(String[] args) {
         BinarySort binarySort = new BinarySort();
         binarySort.runTests();
     }
 
         public int findLowIndex (List<Integer> arr, int key) {
+            // Find the first occurrence of 'key' in the input list of integers.
+            // Perform a binary search by determining if the middle array entry
+            // is of greater or lesser value than the key and repeating that
+            // calculation to determine the first occurrence of the key.
             int low = 0;
             int high = arr.size() - 1;
             int mid = high / 2;
 
             while (low <= high) {
-
                 int mid_elem = arr.get(mid);
 
                 if (mid_elem < key) {
@@ -25,7 +32,6 @@ public class BinarySort {
                 else {
                     high = mid - 1;
                 }
-
                 mid = low + (high - low) / 2;
             }
 
@@ -37,6 +43,10 @@ public class BinarySort {
         }
 
         public int findHighIndex(List<Integer> arr, int key) {
+            // Find the last occurrence of 'key' in the input list of integers.
+            // Perform a binary search by determining if the middle array entry
+            // is of greater or lesser value than the key and repeating that
+            // calculation to determine the last occurrence of the key.
             int low = 0;
             int high = arr.size() - 1;
             int mid = high / 2;
@@ -51,7 +61,6 @@ public class BinarySort {
                 else {
                     high = mid - 1;
                 }
-
                 mid = low + (high - low) / 2;
             }
 
@@ -59,7 +68,7 @@ public class BinarySort {
                 return high;
             }
 
-            if (high < arr.size() && arr.get(high) == key) {
+            if (arr.get(high) == key) {
                 return high;
             }
 
@@ -68,10 +77,17 @@ public class BinarySort {
 
     public void runTests() {
 
-        List<Integer> array = Arrays.asList(1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4,4, 5, 5, 5, 6, 6, 6, 6, 6, 6);
-        int key = 5;
+        List<Integer> array = Arrays.asList(1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6);
+
+        int key = 4;
         int low = findLowIndex(array, key);
         int high = findHighIndex(array, key);
+        System.out.println("Low Index of " + key + ": " + low);
+        System.out.println("High Index of " + key + ": " + high);
+
+        key = 5;
+        low = findLowIndex(array, key);
+        high = findHighIndex(array, key);
         System.out.println("Low Index of " + key + ": " + low);
         System.out.println("High Index of " + key + ": " + high);
 
--- src/com/griffin/utility/BinaryTreeNode.java
+++ src/com/griffin/utility/BinaryTreeNode.java
@@ -1,37 +1,61 @@
 package com.griffin.utility;
 
+/*
+Single node class used to construct a binary tree. A binary tree has at most two leaf nodes,
+ */
 public class BinaryTreeNode {
 
-    private int key;
+    private final int key;
     private BinaryTreeNode leftChild = null;
     private BinaryTreeNode rightChild = null;
 
+    /*
+    Construct a node with a key and no children
+     */
     public BinaryTreeNode(int key) {
         this.key = key;
     }
 
+    /*
+    Construct a node with a key and two children.
+     */
     public BinaryTreeNode(int key, BinaryTreeNode leftChild, BinaryTreeNode rightChild) {
         this.key = key;
         this.leftChild = leftChild;
         this.rightChild = rightChild;
     }
 
+    /*
+    Set left child node.
+     */
     public void setLeftChild(BinaryTreeNode child) {
         this.leftChild = child;
     }
 
+    /*
+    Set right child node.
+     */
     public void setRightChild(BinaryTreeNode child) {
         this.rightChild = child;
     }
 
+    /*
+     Get right child node or null if none exist.
+     */
     public BinaryTreeNode getRightChild() {
         return rightChild;
     }
 
+    /*
+      Get left child node or null if none exist.
+      */
     public BinaryTreeNode getLeftChild() {
         return leftChild;
     }
 
+    /*
+    Get this node's key.
+     */
     public int getKey() {
         return key;
     }
